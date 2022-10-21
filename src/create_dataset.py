61e6d46fe6ede1d4c02b294b5e301bc93764f4ea
Sathish KS <sathish_k_s@hcl.com>
2022-10-21 17:47:31 +0530

    modified create dataset source file path temporarily

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -6,7 +6,7 @@ from config import Config
 Config.ORIGINAL_DATASET_FILE_PATH.parent.mkdir(parents= True, exist_ok= True)
 Config.CLEANED_DATASET_FILE_PATH.mkdir(parents=True, exist_ok=True)
 
-df = pd.read_csv(str(Config.ORIGINAL_DATASET_FILE_PATH))
+df = pd.read_csv(str(Config.CLEANED_DATASET_FILE_PATH/"dataset.csv"))
 #For this use case, we will read the existing data from the original folder.
 #If we are reading the data from a link, we can save the raw dataset from git/kaggle/etc. to raw data folder by the below code
         #df = pd.read_csv ("********URL LINK******")
@@ -15,4 +15,4 @@ df = pd.read_csv(str(Config.ORIGINAL_DATASET_FILE_PATH))
 
 # The raw data is already cleaned and hence no processing is required.
 # We will directly export the dataset from raw_data folder to dataset folder
-df.to_csv (str(Config.CLEANED_DATASET_FILE_PATH/"dataset.csv"), index = False)
+df.to_csv (str(Config.CLEANED_DATASET_FILE_PATH/"dataset_dummy.csv"), index = False)
