6a6fb1fbebd6866bf693e63f4eed9959f4d0a5e5
Sathish KS <sathish_k_s@hcl.com>
2022-10-21 17:16:59 +0530

    updated codes

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 class Config:
     DATA_PATH = Path("./data")
-    RAW_DATASET_FILE_PATH = DATA_PATH/"raw_dataset"/ "clean_data.csv"
+    ORIGINAL_DATASET_FILE_PATH = DATA_PATH/"original_dataset"/ "clean_data.csv"
     CLEANED_DATASET_FILE_PATH = DATA_PATH/ "cleaned_data"
     ANOMALY_TREATED_DATAS_FILE_PATH = DATA_PATH/"anomaly_treated" 
     FEATURES_PATH = DATA_PATH/ "features"
